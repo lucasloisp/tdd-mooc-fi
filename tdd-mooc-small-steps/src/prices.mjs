32f00b5733c48a7a25b27e870e5aab5812d1f8fd
Lucas Lois <lucasloisp@gmail.com>
2023-01-20 19:55:01 -0300

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -4,7 +4,7 @@ import express from "express";
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
 
-const convert = (date) => (true) ? date : date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate()
+const convert = (date) =>  date
 function createApp(database) {
   const app = express();
 
