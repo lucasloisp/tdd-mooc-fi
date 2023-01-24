7aeb4e905fef276d8ea4ed950a74729ec9469525
Lucas Lois <lucasloisp@gmail.com>
2023-01-24 20:40:05 -0300

    implements shape rotation

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -2,13 +2,44 @@ export class RotatingShape {
   shape;
 
   constructor(shapeString) {
-    this.shape = shapeString.split("\n").map((line) => line.trim().split(""));
+    this.shape = shapeString
+      .trim()
+      .split("\n")
+      .map((line) => line.trim().split(""));
   }
 
   toString() {
-    return this.shape
-      .map((row) => row.join(""))
-      .concat("")
-      .join("\n");
+    return stringifyMatrix(this.shape);
   }
+
+  rotateRight() {
+    const shape = this.shape;
+    const height = shape.length;
+    const width = shape[0].length;
+    const matrix = range(width).map((row) =>
+      range(height).map((col) => shape[height - 1 - col][row])
+    );
+    return new RotatingShape(stringifyMatrix(matrix));
+  }
+
+  rotateLeft() {
+    const shape = this.shape;
+    const height = shape.length;
+    const width = shape[0].length;
+    const matrix = range(width).map((row) =>
+      range(height).map((col) => shape[col][height - 1 - row])
+    );
+    return new RotatingShape(stringifyMatrix(matrix));
+  }
+}
+
+function range(size) {
+  return [...Array(size).keys()];
+}
+
+function stringifyMatrix(matrix) {
+  return matrix
+    .map((row) => row.join(""))
+    .concat("")
+    .join("\n");
 }
