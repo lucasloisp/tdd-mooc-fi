0e1ee7b0e5ce099fb7b99d7ff92f189d17fb8f16
Lucas Lois <lucasloisp@gmail.com>
2023-01-23 21:25:39 -0300

    completes Falling Block tests

--- tdd-mooc-tetris/src/Block.mjs
+++ tdd-mooc-tetris/src/Block.mjs
@@ -1,7 +1,13 @@
 export class Block {
   color;
+  depth;
 
   constructor(color) {
     this.color = color;
+    this.depth = 0;
+  }
+
+  tick() {
+    this.depth += 1;
   }
 }
--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,13 +1,44 @@
 export class Board {
   width;
   height;
+  _falling = null;
+  _pieces = [];
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 
+  drop(block) {
+    if (this._falling) {
+      throw new Error("already falling");
+    }
+    this._falling = block;
+  }
+
+  hasFalling() {
+    return !!this._falling;
+  }
+
+  tick() {
+    const falling = this._falling;
+    if (falling.depth === 2 - this._pieces.length) {
+      this._falling = null;
+      this._pieces.push(falling);
+    } else {
+      falling.tick();
+    }
+  }
+
   toString() {
-    return "TODO";
+    const falling = this._falling;
+    const tiles = [[".", ".", "."], [".", ".", "."], [".", ".", "."], []];
+    if (falling) {
+      tiles[falling.depth][1] = falling.color;
+    }
+    this._pieces.forEach((piece) => {
+      tiles[piece.depth][1] = piece.color;
+    });
+    return tiles.map((row) => row.join("")).join("\n");
   }
 }
